e9ceba5778b8317a45091d019a13f7c083e7c438
Taweechai Yuenyang <krumii.it@gmail.com>
2023-08-24 16:53:14 +0700

    update new order generate

--- approve_orders/models/models.py
+++ approve_orders/models/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
 from odoo import models, fields, api
 
 
@@ -25,7 +26,7 @@ class OrderHeader(models.Model):
         'vcsgroup.order_type', string="Order Type", required=True)
     order_date = fields.Date(
         string="Date", default=lambda self: fields.Date.today())
-    name = fields.Char(size=15, string="Order No.", required=True)
+    name = fields.Char(size=15, string="Order No.")
     partner_id = fields.Many2one("res.partner", string="Partner")
     item_count = fields.Integer(string="Item", default="0")
     vat_total = fields.Float(string="Vat.", default="0.0")
@@ -40,11 +41,33 @@ class OrderHeader(models.Model):
     
     @api.model
     def create(self, data):
-        orderNo = data['name']
-        data['name'] = str(orderNo).upper()
+        # orderNo = data['name']
+        orderNo = f"ORD{data['order_date'].strftime('%Y%m%d')}"
+        print(orderNo)
+        # data['name'] = str(orderNo).upper()
+        dte = datetime.now()
+        data['name'] = orderNo
+        data['item_count'] = len(data['line_ids'])
         result = super().create(data)
         print(result)
         return result
+    
+    @api.onchange('line_ids')
+    def onchange_line_ids(self):
+        print(self.line_ids)
+        vatCount = 0
+        for r in self.line_ids:
+            print(r['product_id']['price'])
+            vatCount += float(r['product_id']['price'])
+
+        self.vat_total = vatCount
+        self.item_count = len(self.line_ids)
+
+    # @api.ondelete(self)
+    # def ondelete_order_header(self):
+    #     # self.line_ids.unlink()
+    #     pass
+
 
 
 class OrderDetail(models.Model):
@@ -54,4 +77,9 @@ class OrderDetail(models.Model):
     order_id = fields.Many2one('approve_orders.order_header', string="Order")
     product_id = fields.Many2one('vcsgroup.product_group', string="Product", required=True)
     quantity = fields.Float(string="Quantity", default="0.0")
+    price = fields.Float(string="Price", default="0.0")
     unit_id = fields.Many2one('vcsgroup.unit', string="Unit", required=True)
+
+    @api.onchange('product_id')
+    def onchange_product_id(self):
+        self.price = float(self.product_id.price)
